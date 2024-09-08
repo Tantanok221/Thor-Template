bd63650433ec5555a1fd549e24ec7fb67863359e
Tantanok221 <6572withspicy@gmail.com>
2024-09-08 14:57:37 +0800

    feat: Modify Backend For RPC Supports

--- be/template/src/index.ts
+++ be/template/src/index.ts
@@ -1,36 +1,35 @@
 import { Env, Hono } from "hono";
 import { logger } from "hono/logger";
-
 import { clerkMiddleware } from "@hono/clerk-auth";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { BACKEND_BINDING } from "@pkg/env/constant";
 import UserRoutes from "./routes/users/users";
+import { cors } from "hono/cors";
 
 const app = new Hono<{ Bindings: BACKEND_BINDING }>();
 // app.use("*", clerkMiddleware());
 app.use("*", logger());
 app.use("*", trimTrailingSlash());
+app.use("*", cors())
 const routes = app
   .route("/users", UserRoutes)
 
-export type TemplateBackend = typeof routes;
-
-export default {
-  /** this part manages cronjobs */
-  scheduled(
-    event: ScheduledEvent,
-    env: {
-      DATABASE_URL?: any;
+  
+  export default {
+    /** this part manages cronjobs */
+    scheduled(
+      event: ScheduledEvent,
+      env: BACKEND_BINDING,
+      ctx: ExecutionContext
+    ) {
+      const delayedProcessing = async () => {
+        console.log("CRONJOB EXECUTED");
+      };
+      ctx.waitUntil(delayedProcessing());
     },
-    ctx: ExecutionContext
-  ) {
-    const delayedProcessing = async () => {
-      console.log("CRONJOB EXECUTED");
-    };
-    ctx.waitUntil(delayedProcessing());
-  },
-  fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    return app.fetch(request, env, ctx);
-  },
-  /** this part manages regular REST */
-};
+    fetch(request: Request, env: Env, ctx: ExecutionContext) {
+      return app.fetch(request, env, ctx);
+    },
+    /** this part manages regular REST */
+  };
+export type TemplateBackend = typeof routes;
